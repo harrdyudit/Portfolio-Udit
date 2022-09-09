86bf08a33c50a8a8218fedecd4d517b02d9ab8c7
udit <sharmaudit262@gmail.com>
2022-09-09 14:43:49 +0530

    "UpadteSticky Navbar"

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-const mobailBtn=document.querySelector(".mobail-navbar-btn");
-const header=document.getElementById("header")
+const mobailBtn = document.querySelector(".mobail-navbar-btn");
+const header = document.getElementById("header");
 
 // const sticky=header.offsetTop;
 
-const toggleMenu=()=>{
-    header.classList.toggle("active")
-    
-    
-    
-    
-}
-mobailBtn.addEventListener("click",()=>toggleMenu())
+const toggleMenu = () => {
+  header.classList.toggle("active");
+};
+mobailBtn.addEventListener("click", () => toggleMenu());
 
-window.addEventListener("scroll",function(){
-    const header2=document.getElementById("header")
-    console.log(window.pageXOffset);
-    if(window.pageYOffset>=50){
-        header2.classList.add("sticky")
-    }else{
-        header2.classList.remove("sticky")
-    }
-
-
-})
+window.addEventListener("scroll", function () {
+ 
+  if (window.pageYOffset >= 50) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+});
