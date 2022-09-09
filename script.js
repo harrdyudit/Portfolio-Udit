6d13fcb40a0d66ad8991f710655e154240a19b1a
udit <sharmaudit262@gmail.com>
2022-09-09 14:32:03 +0530

    "sticky Navbar"

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const mobailBtn=document.querySelector(".mobail-navbar-btn");
 const header=document.getElementById("header")
+
+// const sticky=header.offsetTop;
+
 const toggleMenu=()=>{
     header.classList.toggle("active")
-  
     
-
-
+    
+    
+    
 }
 mobailBtn.addEventListener("click",()=>toggleMenu())
+
+window.addEventListener("scroll",function(){
+    const header2=document.getElementById("header")
+    console.log(window.pageXOffset);
+    if(window.pageYOffset>=50){
+        header2.classList.add("sticky")
+    }else{
+        header2.classList.remove("sticky")
+    }
+
+
+})
