edeaf4492029636ebbe48d4112186b447baa0af9
udit <sharmaudit262@gmail.com>
2022-09-18 21:17:59 +0530

    "add Scroll to top btn"

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const mobailBtn = document.querySelector(".mobail-navbar-btn");
 const header = document.getElementById("header");
 
-
-
 const toggleMenu = () => {
   header.classList.toggle("active");
 };
 mobailBtn.addEventListener("click", () => toggleMenu());
 
+
+
+
+// sticky menu-bar
 window.addEventListener("scroll", function () {
- 
   if (window.pageYOffset >= 50) {
     header.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
   }
 });
+
+// scroll to top button
+function goTo(){
+  window.scrollTo(0,0)
+}
